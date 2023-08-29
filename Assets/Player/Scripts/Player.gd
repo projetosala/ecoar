0caf98e9ffee75f6ead389c1ec653dc46f360139
alexcstd <alex.scustod@gmail.com>
2023-08-29 16:21:09 -0300

    adicionando penúltima fase do jogo

--- Assets/Player/Scripts/Player.gd
+++ Assets/Player/Scripts/Player.gd
@@ -22,8 +22,6 @@ func removeHeart(life):
 		get_tree().reload_current_scene()
 		
 func _physics_process(delta):
-	print("hitted", hitted)
-	print(life)
 	motion.y += gravity
 	var friction = false
 	if canMove == true:
